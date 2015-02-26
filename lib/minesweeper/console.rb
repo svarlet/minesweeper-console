95bf96fc5640110edcbdfe6ff19041f584224402
Sébastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-02-26 17:53:08 +0000

    Creates the skeleton of the console based minesweeper.

---
+++ lib/minesweeper/console.rb
@@ -0,0 +1,7 @@
+require "minesweeper/console/version"
+
+module Minesweeper
+  module Console
+    # Your code goes here...
+  end
+end
---
+++ lib/minesweeper/console/version.rb
@@ -0,0 +1,5 @@
+module Minesweeper
+  module Console
+    VERSION = "0.0.1"
+  end
+end
