b7236b3f62dd245078f616357575babbdd3a0ce5
Sébastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-05-11 18:08:15 +0100

    Finishes implementation of the row printer.

--- lib/minesweeper/console/prettyprinter/row_printer.rb
+++ lib/minesweeper/console/prettyprinter/row_printer.rb
@@ -7,12 +7,20 @@ module Minesweeper
           @separator = separator
         end
 
-        def print(row_number, raw_row)
-          result = row_number.to_s + @separator
+        def print(row_number, raw_row, column_width)
+          raise StandardError if column_width.nil?
+          raise StandardError if column_width < 1
+          raise StandardError if column_width < row_number.to_s.length
+          result = prepend_spaces_to(row_number.to_s, column_width) + @separator
           raw_row.each_char do |c|
-            result << c + @separator
+            result << prepend_spaces_to(c, column_width) + @separator
           end
           result
+        end
+
+        def prepend_spaces_to(a_string, desired_length)
+          missing_spaces = desired_length - a_string.length
+          ' ' * missing_spaces + a_string
         end
       end
     end
--- test/minesweeper/console/prettyprinter/row_printer_test.rb
+++ test/minesweeper/console/prettyprinter/row_printer_test.rb
@@ -15,20 +15,39 @@ module Minesweeper
           assert_raise(StandardError) { RowPrinter.new(nil) }
         end
 
+        def test_print_raises_error_when_column_width_is_nil
+          assert_raise(StandardError) { @printer.print(0, 'XYZ', nil) }
+        end
+
+        def test_print_raises_error_when_columns_width_is_not_strictly_positive
+          assert_raise(StandardError) { @printer.print(0, 'XYZ', 0) }
+          assert_raise(StandardError) { @printer.print(0, 'XYZ', -1) }
+        end
+
+        def test_print_raises_error_when_desired_column_width_is_smaller_than_row_number
+          assert_raise(StandardError) { @printer.print(10, 'XYZ', 1) }
+        end
+
         def test_prints_row_number_first
-          assert_match(/^0.*$/, @printer.print(0, 'HHHH'))
+          assert_match(/^0.*$/, @printer.print(0, 'HHHH', 1))
         end
 
         def test_prints_separator_after_row_number
-          assert_match(/^\d+#{SEP}.*$/, @printer.print(0, 'HHHH'))
+          assert_match(/^\d+#{SEP}.*$/, @printer.print(0, 'HHHH', 1))
         end
 
         def test_prints_each_character_of_the_sequence_separated_by_separator
-          assert_match(/^\d+#{SEP}X#{SEP}Y#{SEP}Z.*$/, @printer.print(0, 'XYZ'))
+          assert_match(/^\d+#{SEP}X#{SEP}Y#{SEP}Z.*$/, @printer.print(0, 'XYZ', 1))
         end
 
-        def test_appends_separator
-          assert_match(/^.*#{SEP}$/, @printer.print(0, 'XYZ'))
+        def test_print_appends_separator_at_end_of_line
+          assert_match(/^.*#{SEP}$/, @printer.print(0, 'XYZ', 1))
+        end
+
+        def test_enforces_column_width_on_each_column
+          result = @printer.print(0, 'XYZ', 3)
+          cells = result.split(SEP)
+          cells.each { |cell| assert_equal(3, cell.length) }
         end
       end
     end
