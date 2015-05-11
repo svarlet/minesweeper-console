4c062bbb28f6c683de3da3714379e863b57e5d1b
Sébastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-05-11 10:25:58 +0100

    WIP: Implementing the header printer of the pretty printer.

--- lib/minesweeper/console/prettyprinter/header_printer.rb
+++ lib/minesweeper/console/prettyprinter/header_printer.rb
@@ -4,11 +4,13 @@ module Minesweeper
       class HeaderPrinter
         def initialize(separator)
           raise StandardError if separator.nil?
+          @separator = separator
         end
 
         def print(number_of_columns)
           raise StandardError if number_of_columns == 0
           raise StandardError if number_of_columns < 0
+          result = ' ' * number_of_columns.to_s.length + @separator
         end
       end
     end
--- test/minesweeper/console/prettyprinter/header_printer_test.rb
+++ test/minesweeper/console/prettyprinter/header_printer_test.rb
@@ -7,18 +7,30 @@ module Minesweeper
       class HeaderPrinterTest < Test::Unit::TestCase
         SEP = '§'
 
+        def setup
+          @printer = HeaderPrinter.new(SEP)
+        end
+
         def test_raises_error_when_initialized_with_nil_separator
           assert_raise(StandardError) { HeaderPrinter.new(nil) }
         end
 
         def test_raises_error_when_number_of_columns_is_zero
-          printer = HeaderPrinter.new(SEP)
-          assert_raise(StandardError) { printer.print(0) }
+          assert_raise(StandardError) { @printer.print(0) }
         end
 
         def test_raises_error_when_number_of_columns_is_negative
-          printer = HeaderPrinter.new(SEP)
-          assert_raise(StandardError) { printer.print(-1) }
+          assert_raise(StandardError) { @printer.print(-1) }
+        end
+
+        def test_makes_room_for_the_greatest_row_number
+          assert_match(/^ .*$/, @printer.print(1))
+          assert_match(/^  .*$/, @printer.print(10))
+          assert_match(/^   .*$/, @printer.print(100))
+        end
+
+        def test_inserts_separator_after_first_column
+          assert_match(/^\s+#{SEP}.*$/, @printer.print(1))
         end
       end
     end
