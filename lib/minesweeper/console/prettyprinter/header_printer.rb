1a6f17072748b1b86beb19db7204e79b4c83b898
Sébastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-05-11 14:29:50 +0100

    Implementation of HeaderPrinter.

--- lib/minesweeper/console/prettyprinter/header_printer.rb
+++ lib/minesweeper/console/prettyprinter/header_printer.rb
@@ -10,7 +10,18 @@ module Minesweeper
         def print(number_of_columns)
           raise StandardError if number_of_columns == 0
           raise StandardError if number_of_columns < 0
-          result = ' ' * number_of_columns.to_s.length + @separator
+          column_width = compute_column_width_for(number_of_columns)
+          result = ' ' * column_width + @separator
+          number_of_columns.times do |i|
+            column_header = i.to_s
+            column_header.prepend(' ' * (column_width - column_header.length))
+            result << column_header + @separator
+          end
+          result
+        end
+        
+        def compute_column_width_for(number_of_columns)
+          (number_of_columns - 1).to_s.length
         end
       end
     end
--- test/minesweeper/console/prettyprinter/header_printer_test.rb
+++ test/minesweeper/console/prettyprinter/header_printer_test.rb
@@ -25,12 +25,30 @@ module Minesweeper
 
         def test_makes_room_for_the_greatest_row_number
           assert_match(/^ .*$/, @printer.print(1))
-          assert_match(/^  .*$/, @printer.print(10))
-          assert_match(/^   .*$/, @printer.print(100))
+          assert_match(/^  .*$/, @printer.print(11))
+          assert_match(/^   .*$/, @printer.print(101))
         end
 
         def test_inserts_separator_after_first_column
           assert_match(/^\s+#{SEP}.*$/, @printer.print(1))
+        end
+
+        def test_first_column_number_is_zero
+          assert_match(/^\s#{SEP}0.*$/, @printer.print(1))
+        end
+
+        def test_first_column_number_is_followed_by_separator
+          assert_match(/^.*#{SEP}$/, @printer.print(1))
+        end
+
+        def test_prints_all_columns_number_upto_but_not_including_the_number_of_columns
+          assert_equal(" #{SEP}0#{SEP}1#{SEP}", @printer.print(2))
+        end
+
+        def test_each_column_is_printed_with_the_width_of_the_greatest_column_number
+          result = @printer.print(100)
+          headers = result.split(SEP)
+          headers.each { |header| assert_equal(2, header.length) }
         end
       end
     end
