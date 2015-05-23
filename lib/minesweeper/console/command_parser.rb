b7f1a758d416c0311e55d4a9b9e0b544315d53df
Sebastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-05-23 17:24:00 +0100

    Starts implementation of a CommandParser to parse user input and return commands to execute on the minefield.

---
+++ lib/minesweeper/console/command_parser.rb
@@ -0,0 +1,14 @@
+module Minesweeper
+  module Console
+    class CommandParser
+      def initialize(a_minefield)
+        raise ArgumentError if a_minefield.nil?
+      end
+
+      def parse(user_input)
+
+      end
+    end
+  end
+end
+
--- lib/minesweeper/console/game_loop.rb
+++ lib/minesweeper/console/game_loop.rb
@@ -5,16 +5,19 @@ require_relative 'prettyprinter/minefield_pretty_printer'
 module Minesweeper
   module Console
     class GameLoop
+      PROMPT = "(R)eveal, (F)lag, (U)nflag <x> <y>\n>"
       def initialize(size)
         @row_count = size
         @minefield = Minefield.new(@row_count)
         @pretty_printer = Console::PrettyPrinter::MinefieldPrettyPrinter.new(@minefield)
+        @command_parser = Console::CommandParser.new(@minefield)
       end
 
       def start
         loop do
           puts @pretty_printer.print
-          command = Readline.readline('> ', true)
+          user_input = Readline.readline(PROMPT, true)
+          @command_parser.parser(user_input).execute
         end
       end
 
