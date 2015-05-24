eed6a3e6009a91ea8095427ad0d2a9f996e774fc
Sebastien Varlet <sebastien.varlet@plumbee.co.uk>
2015-05-24 22:55:40 +0100

    Fixes some code to match the tests requirements.

--- lib/minesweeper/console/parser/flag_command.rb
+++ lib/minesweeper/console/parser/flag_command.rb
@@ -3,6 +3,7 @@ module Minesweeper
     module Parser
       class FlagCommand
         def initialize(a_minefield, x, y)
+          raise ArgumentError unless (x.is_a?(Integer) && y.is_a?(Integer))
           @minefield = a_minefield
           @row_index = x
           @column_index = y
